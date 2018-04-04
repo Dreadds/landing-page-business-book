a01adb81cb3a81419e336a5ce7deb51e72ae4e3e
Kevin Tito <u201411659@upc.edu.pe>
2018-04-04 08:01:16 +0000

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'new_age/index'
+  get 'freelancers/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
